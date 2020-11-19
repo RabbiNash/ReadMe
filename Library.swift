47f3c94e798192404fcbf41810bd76c077c2e68a
Tinashe Makuti <tinashe@baobabcircle.com>
2020-11-19 23:41:32 +0200

    Added Book View, Book Row and Detail View

---
+++ Library.swift
@@ -0,0 +1,8 @@
+//
+//  Library.swift
+//  ReadMe
+//
+//  Created by Tinashe Makuti on 19/11/2020.
+//
+
+import Foundation
---
+++ Views/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  ReadMe
+//
+//  Created by Tinashe Makuti on 19/11/2020.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
