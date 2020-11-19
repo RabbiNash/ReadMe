775d0b5d47315ee2ffbe0bb4f9d9cd87598d66c8
Tinashe Makuti <tinashe@baobabcircle.com>
2020-11-19 22:25:22 +0200

    Added Initial Project Scaffolding

---
+++ ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  ReadMe
+//
+//  Created by Tinashe Makuti on 19/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "book")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80)
+                .font(Font.title.weight(.light))
+                .foregroundColor(.secondary)
+                .padding()
+            
+            Text("Hello, world!")
+                .padding()
+                .font(.title2)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Tinashe Makuti on 19/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
